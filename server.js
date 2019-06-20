3fcd260d3950a5012e0296d1678267d9446bee28
Mourizio Zeland <zelandmourizio@gmail.com>
2019-06-19 20:17:44 -0700

    New

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ app.use(express.static(process.cwd() + "/public"));
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
-mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds241097.mlab.com:41097/heroku_vgtphr02");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+mongoose.connect(MONGODB_URI);
+
+
 var db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
